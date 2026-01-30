dd955300f6e14b31794db393c79fec5f5b03b8da
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2026-01-30 22:42:33 +0900

    feat: 라우팅 설정 및 Navbar 추가, Tailwind 초기 세팅

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+import NotFound from './pages/NotFound.jsx';
+import Home from './pages/Home.jsx';
+import AllProducts from './pages/AllProducts.jsx';
+import NewProduct from './pages/NewProduct.jsx';
+import ProductDetail from './pages/ProductDetail.jsx';
+import MyCart from './pages/MyCart.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      { index: true, path: '/', element: <Home /> },
+      { path: '/products', element: <AllProducts /> },
+      { path: '/products/new', element: <NewProduct /> },
+      { path: '/products/:id', element: <ProductDetail /> },
+      { path: '/carts', element: <MyCart /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
