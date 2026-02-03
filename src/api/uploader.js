b85f810024d26ca51c82aa5dd4918b09971d211d
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2026-02-03 14:55:32 +0900

    feat: Cloudinary 이미지 업로드 연결

---
+++ src/api/uploader.js
@@ -0,0 +1,15 @@
+export async function uploadImage(file) {
+  const data = new FormData();
+  data.append('file', file);
+  data.append('upload_preset', import.meta.env.VITE_CLOUDINARY_PRESET);
+
+  return fetch(import.meta.env.VITE_CLOUDINARY_URL, {
+    method: 'POST',
+    body: data,
+  }) //
+    .then((res) => res.json())
+    .then((data) => {
+      console.log('data:', data);
+      return data.url;
+    });
+}
