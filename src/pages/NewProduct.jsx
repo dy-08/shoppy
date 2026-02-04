8c29fbd438081f7190d2d6074eac730b57cca462
Youngho Kwon <127571276+dy-08@users.noreply.github.com>
2026-02-04 14:14:53 +0900

    refactor: 상품 조회·추가 React Query 로직을 useProducts 훅으로 통합

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,19 +1,15 @@
 import { useState } from 'react';
 import Button from '../components/ui/Button';
 import { uploadImage } from '../api/uploader';
-import { addNewProduct } from '../api/firebase';
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import useProducts from '../hooks/useProducts';
+
 export default function NewProduct() {
   const [product, setProduct] = useState({});
   const [file, setFile] = useState();
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
 
-  const queryClient = useQueryClient();
-  const addProduct = useMutation({
-    mutationFn: ({ product, url }) => addNewProduct(product, url),
-    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['products'] }),
-  });
+  const { addProduct } = useProducts();
 
   const handleChange = (e) => {
     const { name, value, files } = e.target;
